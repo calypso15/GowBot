664e90dd8ec74fe42f9ff5877afa9ea9870ba135
Ryan O'Grady <ryan.ogrady@soartech.com>
2017-02-28 22:15:45 -0500

    Added basic structure for troops

--- src/net/ryanogrady/gowbot/Team.java
+++ src/net/ryanogrady/gowbot/Team.java
@@ -1,8 +1,22 @@
 package net.ryanogrady.gowbot;
 
 public class Team {
-	
+	public final int MAX_TROOPS = 4;
+
+	private Troop[] troops = new Troop[MAX_TROOPS];
+
 	public Team() {
 	}
 
+	public Troop getTroop(int index) {
+		return troops[index];
+	}
+
+	public void addTroop(Troop troop, int index) {
+		troops[index] = troop;
+	}
+
+	public void removeTroop(int index) {
+		troops[index] = null;
+	}
 }
